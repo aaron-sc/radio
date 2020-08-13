786dd93363bed8d9b124ee82a88f77bc8a33af72
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2020-08-13 14:36:45 -0600

    Update install.py

--- install.py
+++ install.py
@@ -4,8 +4,8 @@ import subprocess
 
 # Below installs the FM player
 
-print("Installing required packages")
-os.system("sudo apt update && sudo apt install -y sox make gcc g++ git libmp3lame-dev")
-os.chdir("~")
-print("INSTALLING THE FM TRANSMITTER IN ~")
-os.system("sudo git clone https://github.com/markondej/fm_transmitter && sudo  && cd fm_transmitter && make")
+# print("Installing required packages")
+# os.system("sudo apt update && sudo apt install -y sox make gcc g++ git libmp3lame-dev")
+# os.chdir("~")
+# print("INSTALLING THE FM TRANSMITTER IN ~")
+# os.system("sudo git clone https://github.com/markondej/fm_transmitter && sudo  && cd fm_transmitter && make")
